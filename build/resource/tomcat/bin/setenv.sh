d9df32c2a8a95bcc88a37a266f9880ad56455895
cfmlprojects <cfml@cfmlprojects.org>
2013-04-30 23:29:23 -0600

    first\!

---
+++ build/resource/tomcat/bin/setenv.sh
@@ -0,0 +1,13 @@
+# Tomcat memory settings
+# -Xms<size> set initial Java heap size
+# -Xmx<size> set maximum Java heap size
+# -Xss<size> set java thread stack size
+# -XX:MaxPermSize sets the java PermGen size
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+JAVA_OPTS="-Xms128m -Xmx256m -XX:MaxPermSize=64m";   # memory settings
+JAVA_OPTS="$JAVA_OPTS -javaagent:$DIR/../../lib/railo-inst.jar ";   # java agent settings
+
+# needed by FR
+# LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/opt/railo/fusionreactor/etc/lib
+
+export JAVA_OPTS;
